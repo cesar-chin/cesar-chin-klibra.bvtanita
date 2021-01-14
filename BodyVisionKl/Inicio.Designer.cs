3b62f7a305131a85646e0f0a9e73deeb57311c2c
Cesar Chinchilla M <chinchillamc@hotmail.com>
2021-01-13 21:43:40 -0600

    Cambio de prueba

--- BodyVisionKl/Inicio.Designer.cs
+++ BodyVisionKl/Inicio.Designer.cs
@@ -51,76 +51,85 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 40);
+            this.label1.Location = new System.Drawing.Point(27, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Paciente: ";
             // 
             // txtNombre
             // 
             this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtNombre.Location = new System.Drawing.Point(189, 40);
+            this.txtNombre.Location = new System.Drawing.Point(142, 32);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(327, 13);
+            this.txtNombre.Size = new System.Drawing.Size(263, 13);
             this.txtNombre.TabIndex = 1;
             // 
             // txtEdad
             // 
             this.txtEdad.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtEdad.Location = new System.Drawing.Point(383, 72);
+            this.txtEdad.Location = new System.Drawing.Point(287, 58);
+            this.txtEdad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtEdad.Name = "txtEdad";
-            this.txtEdad.Size = new System.Drawing.Size(45, 13);
+            this.txtEdad.Size = new System.Drawing.Size(34, 13);
             this.txtEdad.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(341, 70);
+            this.label2.Location = new System.Drawing.Point(256, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.Size = new System.Drawing.Size(32, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Edad";
             // 
             // txtFecha
             // 
             this.txtFecha.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtFecha.Location = new System.Drawing.Point(189, 131);
+            this.txtFecha.Location = new System.Drawing.Point(142, 106);
+            this.txtFecha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFecha.Name = "txtFecha";
-            this.txtFecha.Size = new System.Drawing.Size(162, 13);
+            this.txtFecha.Size = new System.Drawing.Size(122, 13);
             this.txtFecha.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 129);
+            this.label3.Location = new System.Drawing.Point(27, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 19);
+            this.label3.Size = new System.Drawing.Size(92, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Fecha del archivo";
             // 
             // txtArchivo
             // 
             this.txtArchivo.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtArchivo.Location = new System.Drawing.Point(189, 157);
+            this.txtArchivo.Location = new System.Drawing.Point(142, 128);
+            this.txtArchivo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtArchivo.Name = "txtArchivo";
-            this.txtArchivo.Size = new System.Drawing.Size(535, 13);
+            this.txtArchivo.Size = new System.Drawing.Size(401, 13);
             this.txtArchivo.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 155);
+            this.label4.Location = new System.Drawing.Point(27, 126);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 15);
+            this.label4.Size = new System.Drawing.Size(87, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Archivo de datos";
             // 
             // btnProcesar
             // 
-            this.btnProcesar.Location = new System.Drawing.Point(593, 389);
+            this.btnProcesar.Location = new System.Drawing.Point(445, 316);
+            this.btnProcesar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnProcesar.Name = "btnProcesar";
-            this.btnProcesar.Size = new System.Drawing.Size(141, 40);
+            this.btnProcesar.Size = new System.Drawing.Size(106, 32);
             this.btnProcesar.TabIndex = 8;
             this.btnProcesar.Text = "Paciente";
             this.btnProcesar.UseVisualStyleBackColor = true;
@@ -128,9 +137,10 @@
             // 
             // btnMostrar
             // 
-            this.btnMostrar.Location = new System.Drawing.Point(446, 389);
+            this.btnMostrar.Location = new System.Drawing.Point(334, 316);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMostrar.Name = "btnMostrar";
-            this.btnMostrar.Size = new System.Drawing.Size(141, 40);
+            this.btnMostrar.Size = new System.Drawing.Size(106, 32);
             this.btnMostrar.TabIndex = 9;
             this.btnMostrar.Text = "Perfil";
             this.btnMostrar.UseVisualStyleBackColor = true;
@@ -143,9 +153,10 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(152, 389);
+            this.btnCancelar.Location = new System.Drawing.Point(114, 316);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(141, 40);
+            this.btnCancelar.Size = new System.Drawing.Size(106, 32);
             this.btnCancelar.TabIndex = 10;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -153,9 +164,10 @@
             // 
             // btnData
             // 
-            this.btnData.Location = new System.Drawing.Point(299, 389);
+            this.btnData.Location = new System.Drawing.Point(224, 316);
+            this.btnData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnData.Name = "btnData";
-            this.btnData.Size = new System.Drawing.Size(141, 40);
+            this.btnData.Size = new System.Drawing.Size(106, 32);
             this.btnData.TabIndex = 11;
             this.btnData.Text = "Data";
             this.btnData.UseVisualStyleBackColor = true;
@@ -163,43 +175,47 @@
             // txtGenero
             // 
             this.txtGenero.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtGenero.Location = new System.Drawing.Point(189, 100);
+            this.txtGenero.Location = new System.Drawing.Point(142, 81);
+            this.txtGenero.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtGenero.Name = "txtGenero";
-            this.txtGenero.Size = new System.Drawing.Size(130, 13);
+            this.txtGenero.Size = new System.Drawing.Size(98, 13);
             this.txtGenero.TabIndex = 13;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(36, 98);
+            this.label5.Location = new System.Drawing.Point(27, 80);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(48, 15);
+            this.label5.Size = new System.Drawing.Size(42, 13);
             this.label5.TabIndex = 12;
             this.label5.Text = "Género";
             // 
             // txtFechaNacimiento
             // 
             this.txtFechaNacimiento.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtFechaNacimiento.Location = new System.Drawing.Point(189, 72);
+            this.txtFechaNacimiento.Location = new System.Drawing.Point(142, 58);
+            this.txtFechaNacimiento.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFechaNacimiento.Name = "txtFechaNacimiento";
-            this.txtFechaNacimiento.Size = new System.Drawing.Size(130, 13);
+            this.txtFechaNacimiento.Size = new System.Drawing.Size(98, 13);
             this.txtFechaNacimiento.TabIndex = 15;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(36, 70);
+            this.label6.Location = new System.Drawing.Point(27, 57);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(122, 15);
+            this.label6.Size = new System.Drawing.Size(106, 13);
             this.label6.TabIndex = 14;
             this.label6.Text = "Fecha de nacimiento";
             // 
             // Inicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.ClientSize = new System.Drawing.Size(586, 368);
             this.Controls.Add(this.txtFechaNacimiento);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.txtGenero);
@@ -218,6 +234,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Inicio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
